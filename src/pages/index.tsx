a54d0cddd9f049efc4c86c643cf2427e8d227487
Caio Vinícius <caio1525pereira@gmail.com>
2023-01-02 23:14:14 -0300

    feat: :tada: Base do projeto e configurações iniciais
    Setup & estrutura - Criação do projeto em Next

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ignite Call</title>
+      </Head>
+      <main>
+        <h1> Ignite Call </h1>
+      </main>
+    </>
+  );
+}
