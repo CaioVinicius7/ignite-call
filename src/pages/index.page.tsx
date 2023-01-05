ac70daf3c44d18844bfc862dde9c17b150a4166b
Caio Vinícius <caio1525pereira@gmail.com>
2023-01-05 12:43:53 -0300

    feat: :lipstick: Estrutura visual da página Home
    Cadastro de usuário - Estrutura visual da home

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,15 +1,1 @@
-import Head from "next/head";
-import { Heading } from "@ignite-ui/react";
-
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Ignite Call</title>
-      </Head>
-      <main>
-        <Heading> Ignite Call </Heading>
-      </main>
-    </>
-  );
-}
+export { default } from "./home";
