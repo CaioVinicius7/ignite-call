2f2c44634988e59f02d3baffe4923dd8d02db282
Caio Vinícius <caio1525pereira@gmail.com>
2023-01-08 01:21:51 -0300

    feat: :safety_vest: Validação de dados do formulário da página Home
    Cadastro de usuário - Validação com Zod

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, TextInput } from "@ignite-ui/react";
 import { ArrowRight } from "phosphor-react";
 
@@ -6,13 +7,23 @@ import { Form } from "./styles";
 import { z } from "zod";
 
 const claimUsernameFormSchema = z.object({
-  username: z.string()
+  username: z
+    .string()
+    .min(3, {
+      message: "O usuário precisa ter pelo menos 3 letras."
+    })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: "O usuário pode ter apenas letras e hifens."
+    })
+    .transform((username) => username.toLowerCase())
 });
 
 type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>;
 
 export function ClaimUsernameForm() {
-  const { register, handleSubmit } = useForm<ClaimUsernameFormData>();
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema)
+  });
 
   async function handleClaimUsername(data: ClaimUsernameFormData) {
     console.log(data);
