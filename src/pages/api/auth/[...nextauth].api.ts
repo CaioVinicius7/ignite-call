2e19ed1f5123019852c2f0937249f30d1b1d56f0
Caio Vinícius <caio1525pereira@gmail.com>
2023-01-17 18:35:04 -0300

    feat: :sparkles: Callback de conteúdo da sessão do Next Auth
    Conexão com calendário - Conteúdo da sessão do Next Auth

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -40,6 +40,12 @@ export function buildNextAuthOptions(
         }
 
         return true;
+      },
+      async session({ session, user }) {
+        return {
+          ...session,
+          user
+        };
       }
     },
     secret: process.env.NEXT_AUTH_SECRET
