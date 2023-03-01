8dc3fbfe081c118fa82eb963f7c150e38668302f
Caio Vinícius <caio1525pereira@gmail.com>
2023-03-01 18:50:42 -0300

    feat: :sparkles: Utilizando tanstack query para realizar query de horários disponíveis
    Calendário e disponibilidade - Configurando React Query

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 
 import {
@@ -21,7 +22,6 @@ interface Availability {
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-  const [availability, setAvailability] = useState<Availability | null>(null);
 
   const router = useRouter();
 
@@ -34,21 +34,25 @@ export function CalendarStep() {
     ? dayjs(selectedDate).format("DD [de] MMMM")
     : null;
 
-  useEffect(() => {
-    if (!selectedDate) {
-      return;
-    }
+  const selectedDateWithoutTime = selectedDate
+    ? dayjs(selectedDate).format("YYYY-MM-DD")
+    : null;
 
-    api
-      .get(`/users/${username}/availability`, {
+  const { data: availability } = useQuery<Availability>(
+    ["availability", selectedDateWithoutTime],
+    async () => {
+      const { data } = await api.get(`/users/${username}/availability`, {
         params: {
-          date: dayjs(selectedDate).format("YYYY-MM-DD")
+          date: selectedDateWithoutTime
         }
-      })
-      .then((response) => {
-        setAvailability(response.data);
       });
-  }, [selectedDate, username]);
+
+      return data;
+    },
+    {
+      enabled: !!selectedDate
+    }
+  );
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
